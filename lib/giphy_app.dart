943f8d2d5540b83f226b5e2c45ca671809e60e63
MatveevYasha <zif666ne@gmail.com>
2025-11-16 18:14:59 +0100

    🎉 Make project skeleton

---
+++ lib/giphy_app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:giphy_service/home_page/Home_page.dart';
+
+class GiphyApp extends StatelessWidget {
+  const GiphyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple)),
+      home: const HomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:giphy_service/giphy_app.dart';
+
+void main() {
+  runApp(const GiphyApp());
+}
